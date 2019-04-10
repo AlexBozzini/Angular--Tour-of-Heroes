7cb39206c9cfe3dfc9a09d5d18118a9638348492
Alexandra Bozzini <alexandrabozzini@zipcoders-MacBook-Pro-74.local>
2019-04-10 15:55:56 -0400

    Finished routing

--- src/app/hero-service/hero.service.ts
+++ src/app/hero-service/hero.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Hero } from 'src/app/heroes/hero';
 import { HEROES } from 'src/app/heroes/mock-heroes';
 import { Observable, of } from 'rxjs';
-import { MessageService } from './message.service';
+import { MessageService } from '../message-service/message.service';
 
 
 @Injectable({
@@ -13,8 +13,12 @@ export class HeroService {
   constructor(private messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]> {
-    // TODO: send the message _after_ fetching the heroes
     this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
+  }
+
+  getHero(id: number): Observable<Hero> {
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of (HEROES.find(hero => hero.id === id));
   }
 }
